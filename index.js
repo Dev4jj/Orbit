bf8429c7daa4c19fe720701fa32e8a7ce8346a81
Dev4jj <Ishaya.jj07@gmail.com>
2024-10-14 15:44:32 -0700

    created an account update modal and created new routes for user posts, account updates, and a loggout route

--- index.js
+++ index.js
@@ -17,25 +17,49 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 
-
+let access = true; // check databse if user info matches then set access to true or false
 
 app.get('/', (req, res)=>{
-    res.render("index");
+    res.render("index", {access});
 })
 
 // check if user already exists in future database
 app.post('/signup', (req, res)=>{
-    res.render("index");
+    res.redirect("index");
     console.log("Post was called");
     console.log(req.body);
 });
 
 app.get('/login', (req, res)=>{
-    const access = false; // check databse if user info matches then set access to true or false
+    access = true;
     res.render("index", {access});
     console.log('User is logged in');
     console.log(req.body);
 });
+
+//user makes a post
+
+app.post("/post", (req, res)=>{
+    res.render("index", {access});
+    console.log("User made a post");
+    console.log(req.body);
+})
+
+//user updates profile
+
+app.post("/update", (req, res)=>{
+    res.render("index", {access});
+    console.log("User account updated");
+    console.log(req.body);
+})
+
+//user navigates to loggout
+
+app.get("/loggout", (req, res)=>{
+   access = false;
+    res.render("index", {access});
+    console.log("user logged out");
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);

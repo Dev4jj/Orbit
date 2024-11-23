4f1173c07f60484ce1553e013fc6c2469572302a
Dev4jj <Ishaya.jj07@gmail.com>
2024-11-22 23:19:54 -0800

    cleaning up code

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(process.env.PORT)
 
 //connect to db
 
